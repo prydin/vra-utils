617e03a4379a6a492ff1e549be099ab7497b4899
prydin <prydin@vmware.com>
2020-07-15 15:51:25 -0400

    Fixed password handling

--- cloud-init/ubuntu-18/init.sh
+++ cloud-init/ubuntu-18/init.sh
@@ -2,6 +2,7 @@
 USER=$1
 PASS=$2
 apt update
-echo $USER:$PASS | /usr/sbin/chpasswd
+echo "$USER:$PASS" | /usr/sbin/chpasswd
+echo "$USER:$PASS" | /usr/sbin/chpasswd
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 service ssh reload
